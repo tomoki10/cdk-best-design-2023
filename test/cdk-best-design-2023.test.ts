69740fed14aff568957ffc9ce4eb7ef61a9d5d81
Tomoki Sato <sato.tomoki@HL01352.local>
2023-07-07 00:24:16 +0900

    Initial commit

---
+++ test/cdk-best-design-2023.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBestDesign2023 from '../lib/cdk-best-design-2023-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-best-design-2023-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBestDesign2023.CdkBestDesign2023Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
